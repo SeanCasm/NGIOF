8c6e0149a5f4943220f5ed41c559a69ce290baa8
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-03-21 17:57:42 -0300

    Removed unnused interfaces

--- Assets/Code/Interfaces.cs
+++ Assets/Code/Interfaces.cs
@@ -2,10 +2,3 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IVisualDamageable
-{
-    IEnumerator VisualFeedBack();
-}
-public interface IHealeable{
-    void AddHealth();
-} 
