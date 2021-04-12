564bbf586ce0a178ea669e585c04b3653a2a18c9
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-04-12 19:08:40 -0400

    Removed unnused properties

--- Assets/Code/Props/Ball.cs
+++ Assets/Code/Props/Ball.cs
@@ -9,29 +9,22 @@ public class Ball : MonoBehaviour
     [SerializeField] int damage;
     [SerializeField] float speed;
     [Tooltip("Parent points, child points will increment after parent ball get destroyed")]
-    [SerializeField] int points; 
-    [SerializeField] int ballTier;
+    [SerializeField] int points;
     [Header("Child settings")]
     [SerializeField] int totalChilds;
-    [SerializeField] byte maxParentLevel;
     [SerializeField] AssetReference childBall;
     private List<GameObject> ball;
     private Vector3 direction;
-    private Collider2D trigger;
     private Rigidbody2D rigid;
     private Vector2 lastVelocity;
     int oi;
-    public int Points{get=>points;set=>points=value;}
-    public bool crossedOnSide { get; set; }
+    public int Points { get => points; set => points = value; }
     public byte parentLevel { get; set; } = 0;
-    public int Damage { get => damage; set => damage = value; }
-
     #endregion
     #region Unity Methods
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        trigger = GetComponentInChildren<Collider2D>();
     }
     private void Start()
     {
@@ -49,27 +42,22 @@ public class Ball : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Ground"))
         {
-            if (other.gameObject.name == "onside" && !crossedOnSide) crossedOnSide = true;
-            else
-            {
-                var speed = lastVelocity.magnitude;
-                direction = Vector2.Reflect(lastVelocity.normalized, other.contacts[0].normal);
-                rigid.SetVelocity(direction * Mathf.Max(speed, 0f));
-            }
+            var speed = lastVelocity.magnitude;
+            direction = Vector2.Reflect(lastVelocity.normalized, other.contacts[0].normal);
+            rigid.SetVelocity(direction * Mathf.Max(speed, 0f));
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        switch(other.tag){
+        switch (other.tag)
+        {
             case "Player":
-            other.GetComponentInParent<Game.Player.Health>().AddDamage(damage);
-            break;
+                other.GetComponentInParent<Game.Player.Health>().AddDamage(damage);
+                break;
             case "Bullet":
                 oi++;
-                if(oi==1){
-                    Break();
-                }
-            break;
+                if (oi == 1) Break();
+                break;
         }
     }
     #endregion
@@ -82,23 +70,20 @@ public class Ball : MonoBehaviour
     }
     public void Break()
     {
-        if (parentLevel <= maxParentLevel)
+        if (parentLevel == 0)
         {
-            if(parentLevel==0){
-                BallSpawner.parentBalls--;
-            }
+            BallSpawner.parentBalls--;
             foreach (var item in ball)
             {
                 var ball = Instantiate(item, transform.position, Quaternion.identity, null);
                 var component = ball.GetComponent<Ball>();
-                component.crossedOnSide = true;
                 component.parentLevel++;
-                component.Points+=this.points+1;
+                component.Points += this.points + 1;
                 ball.transform.localScale = new Vector3(transform.localScale.x / 2, transform.localScale.y / 2);
             }
         }
-        int score=ScoreHandler.score+=points;
-        ScoreUIHandler.score.Invoke(score,TierCalculator.tierLvl);
+        int score = ScoreHandler.Score += points;
+        ScoreUIHandler.score.Invoke(score, ScoreHandler.tierLvl);
         BallSpawner.totalBallsRemaining--;
         Destroy(gameObject);
     }
